23e5e178abfd89505c705ef0e70bcfbbfba22d92
Brydon Parker <brydonparker4@gmail.com>
2022-06-17 16:58:42 -0400

    adding fct_factless_follower_attitrbuion

--- models/marts/fct_transaction_interactions_per_tweet_and_follower.sql
+++ models/marts/fct_transaction_interactions_per_tweet_and_follower.sql
@@ -11,6 +11,13 @@ select
     qu.count_quotes,
     re.count_replies,
     ret.count_retweets,
+    case 
+        when 
+            li.count_likes > 0 or 
+            qu.count_quotes > 0 or
+            re.count_replies > 0 or
+            ret.count_retweets > 0
+    then true else false end as is_interaction
 from {{ ref('int_author_follower_combos_per_tweet') }} as ac
 left join {{ ref('stg_twitter__likes') }} as li on
     ac.follower_user_id = li.follower_user_id and 
@@ -24,7 +31,10 @@ left join {{ ref('stg_twitter__replies') }} as re on
 left join {{ ref('stg_twitter__retweets') }} as ret on
     ac.follower_user_id = ret.follower_user_id and 
     ac.tweet_id = ret.tweet_id
-
-
+where 
+    li.count_likes > 0 or 
+    qu.count_quotes > 0 or
+    re.count_replies > 0 or
+    ret.count_retweets > 0
 
     

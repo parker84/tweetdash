277d075badebac8d669204abd45824fe57f63c3b
parker84 <brydonparker4@gmail.com>
2022-06-24 10:10:34 -0400

    bug fix in retention rate calcs

--- models/marts/fct_accumulating_users_followers.sql
+++ models/marts/fct_accumulating_users_followers.sql
@@ -3,6 +3,9 @@
 select 
     duf.author_user_id,
     duf.follower_user_id,
+    --eligible
+    duf.count_eligible_1w_retention as count_eligible_1w_retention,
+    duf.count_eligible_4w_retention as count_eligible_4w_retention,
     --activation
     duf.first_interaction_at as first_active_at,
     sum(trans.active_follower_transitions) as count_is_active, -- 1 or 0
@@ -12,9 +15,6 @@ select
     sum(act.count_replies) as count_replies,
     sum(act.count_retweets) as count_retweets,
     sum(act.count_quotes) as count_quotes,
-    --eligible
-    sum(duf.count_eligible_1w_retention) as count_eligible_1w_retention,
-    sum(duf.count_eligible_4w_retention) as count_eligible_4w_retention,
     --retention
     sum(
         case 
@@ -49,4 +49,4 @@ left join {{ ref('int_weekly_active_followers_transitions') }} as trans using (a
 left join {{ ref('int_weekly_active_followers') }} as act using (author_user_id, follower_user_id, week_begin_date)
 where true
     and trans.week_begin_date < (select max(week_begin_date) from {{ ref('int_weekly_active_followers_transitions') }})
-group by 1,2,3
+group by 1,2,3,4,5
